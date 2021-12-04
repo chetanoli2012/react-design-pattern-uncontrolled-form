6d07f6ae604962503b43dabfdd52ffe2a5e3bdba
Chetan <chetanoli2012@gmail.com>
2021-12-04 22:51:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { UncontrolledForm } from "./UncontrolledForm";
+
+function App() {
+  return (
+    <>
+      <UncontrolledForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/UncontrolledForm.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const UncontrolledForm = () => {
+  const nameInput = React.createRef();
+  const ageInput = React.createRef();
+  const hairColorInput = React.createRef();
+
+  const handleSubmit = (e) => {
+    /**
+     * This is uncontrolled because this component is not really concerned about the
+     * value of each of the input elements unless some event occurs
+     */
+
+    console.log(
+      nameInput.current.value,
+      "\n",
+      ageInput.current.value,
+      "\n",
+      hairColorInput.current.value
+    );
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="name" type="text" placeholder="Name" ref={nameInput} />
+      <input name="age" type="number" placeholder="Age" ref={ageInput} />
+      <input
+        name="hairColor"
+        type="text"
+        placeholder="Hair Color"
+        ref={hairColorInput}
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
